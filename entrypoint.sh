30ea12d33363a89f252b0423410e5b4b11092736
Civitaspo <civitaspo@gmail.com>
2019-05-09 14:05:18 +0900

    Rewrite by modern bash way

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,40 +33,40 @@ declare -ir DUPLICATE_ENI_ATTACHMENT_PER_HOUR_THRESHOLD=${DUPLICATE_ENI_ATTACHME
 
 declare -r INSTANCE_IDENTITY_URL=${INSTANCE_IDENTITY_URL:-http://169.254.169.254/latest/dynamic/instance-identity/document}
 declare -i num_attempts=0
-while [ -z "$(curl -s $INSTANCE_IDENTITY_URL | jq -r .instanceId)" ]; do
+while [[ -z "$(curl -s ${INSTANCE_IDENTITY_URL} | jq -r .instanceId)" ]]; do
     if ((num_attempts == POLLING_MAX_ATTEMPTS)); then
-        __error_log "Max attpempts(=$POLLING_MAX_ATTEMPTS) is exceeded because $INSTANCE_IDENTITY_URL did not become available."
+        __error_log "Max attempts(=$POLLING_MAX_ATTEMPTS) is exceeded because $INSTANCE_IDENTITY_URL did not become available."
         exit 1
     fi
-    __info_log "Wait until $INSTANCE_IDENTITY_URL become available. (attpempts: $((num_attempts++))/$POLLING_MAX_ATTEMPTS)"
-    sleep $POLLING_INTERVAL
+    __info_log "Wait until $INSTANCE_IDENTITY_URL become available. (attempts: $((num_attempts++))/$POLLING_MAX_ATTEMPTS)"
+    sleep ${POLLING_INTERVAL}
 done
 unset num_attempts
 
 declare -r INSTANCE_ID=$(curl -s ${INSTANCE_IDENTITY_URL} | jq -r .instanceId)
-declare -r REGION=$(curl -s $INSTANCE_IDENTITY_URL | jq -r .region)
+declare -r REGION=$(curl -s ${INSTANCE_IDENTITY_URL} | jq -r .region)
 declare -r AUTOSCALING_GROUP_NAME=$(
     aws ec2 describe-tags \
-        --region $REGION \
+        --region ${REGION} \
         --filters "Name=resource-id,Values=$INSTANCE_ID" \
                   "Name=key,Values=aws:autoscaling:groupName" \
         | jq -r '.Tags[0].Value'
     )
-if [ -z "$AUTOSCALING_GROUP_NAME" ]; then
+if [[ -z "$AUTOSCALING_GROUP_NAME" ]]; then
     __error_log "Unable to fetch the autoscaling group that ec2:$INSTANCE_ID joins in."
     exit 1
 fi
 
 declare -r  TERMINATE_STALE_EC2=${TERMINATE_STALE_EC2:-true}
-if [ "$TERMINATE_STALE_EC2" = true ]; then
+if [[ "$TERMINATE_STALE_EC2" = true ]]; then
     __info_log "Set TERMINATE_STALE_EC2=true"
 else
     __info_log "Set TERMINATE_STALE_EC2=false"
 fi
 
 declare -r  ENTER_STANDBY_STALE_EC2=${ENTER_STANDBY_STALE_EC2:-false}
-if [ "$ENTER_STANDBY_STALE_EC2" = true ]; then
-    if [ "$TERMINATE_STALE_EC2" = true ]; then
+if [[ "$ENTER_STANDBY_STALE_EC2" = true ]]; then
+    if [[ "$TERMINATE_STALE_EC2" = true ]]; then
         __warn_log "Ignore ENTER_STANDBY_STALE_EC2 setting because of TERMINATE_STALE_EC2=true."
     else
         __info_log "Set ENTER_STANDBY_STALE_EC2=true"
@@ -76,8 +76,8 @@ else
 fi
 
 declare -r  DETACH_STALE_EC2=${DETACH_STALE_EC2:-false}
-if [ "$DETACH_STALE_EC2" = true ]; then
-    if [ "$TERMINATE_STALE_EC2" = true ]; then
+if [[ "$DETACH_STALE_EC2" = true ]]; then
+    if [[ "$TERMINATE_STALE_EC2" = true ]]; then
         __warn_log "Ignore DETACH_STALE_EC2 setting because of TERMINATE_STALE_EC2=true."
     else
         __info_log "Set DETACH_STALE_EC2=true"
@@ -86,31 +86,31 @@ else
     __info_log "Set TERMINATE_STALE_EC2=false"
 fi
 
-declare -r  ECS_CLUSTER="$(curl -s $ECS_CONTAINER_METADATA_URI/task | jq -r .Cluster)"
-if [ -z "$ECS_CLUSTER" ]; then
+declare -r  ECS_CLUSTER="$(curl -s ${ECS_CONTAINER_METADATA_URI}/task | jq -r .Cluster)"
+if [[ -z "$ECS_CLUSTER" ]]; then
     __error_log "Unable to fetch the name of the cluster."
     exit 1
 fi
 declare -r  ATTRIBUTES_FOR_STALE_EC2="${ATTRIBUTES_FOR_STALE_EC2:-}"
 declare -ir MAX_ATTRIBUTES=${MAX_ATTRIBUTES:-10}  # AWS Restriction
 declare -a  ATTRIBUTES_FOR_AWSCLI=()
-if [ -n "$ATTRIBUTES_FOR_STALE_EC2" ]; then
+if [[ -n "$ATTRIBUTES_FOR_STALE_EC2" ]]; then
     declare -r container_instance_arn=$(
         aws ecs list-container-instances \
-            --region $REGION \
-            --cluster $ECS_CLUSTER \
+            --region ${REGION} \
+            --cluster ${ECS_CLUSTER} \
             --filter "ec2InstanceId==$INSTANCE_ID" \
             | jq -r '.containerInstanceArns[0]'
         )
-    if [ -z "container_instance_arn" ]; then
+    if [[ -z "container_instance_arn" ]]; then
         __error_log "Unable to fetch the arn of the container instance(ec2 id: $INSTANCE_ID)."
         exit 1
     fi
     __info_log "Container Instance ARN: $container_instance_arn"
 
     declare -i num_attrs=0
-    for attr_kv in $ATTRIBUTES_FOR_STALE_EC2; do
-        if [ "$((++num_attrs))" -gt $MAX_ATTRIBUTES ]; then
+    for attr_kv in ${ATTRIBUTES_FOR_STALE_EC2}; do
+        if [[ "$((++num_attrs))" -gt ${MAX_ATTRIBUTES} ]]; then
             __error_log "You can specify up to $MAX_ATTRIBUTES attributes in a single call."
             exit 1
         fi
@@ -151,7 +151,7 @@ while true; do
             break 2
         fi
     done
-    sleep $POLLING_INTERVAL
+    sleep ${POLLING_INTERVAL}
 done
 
 declare -a actions=()
@@ -177,7 +177,7 @@ unset actions
 declare -r SLACK_MESSAGE=":hammer_and_wrench: $MESSAGE $SLACK_ADDITIONAL_MESSAGE"
 __warn_log "$MESSAGE"
 
-if [ ! -z "$SLACK_URL" ]; then
+if [[ ! -z "$SLACK_URL" ]]; then
     curl -X POST \
          -H 'Content-type: application/json' \
          --data "
@@ -188,37 +188,37 @@ if [ ! -z "$SLACK_URL" ]; then
                  \"username\":\"$APP_NAME\"
              }
              " \
-         $SLACK_URL
+         ${SLACK_URL}
 fi
 
 # Put attributes
-if [ "${#ATTRIBUTES_FOR_AWSCLI[@]}" -ne 0 ]; then
+if [[ "${#ATTRIBUTES_FOR_AWSCLI[@]}" -ne 0 ]]; then
     aws ecs put-attributes \
-        --cluster $ECS_CLUSTER \
+        --cluster ${ECS_CLUSTER} \
         --attributes ${ATTRIBUTES_FOR_AWSCLI[@]} \
-        --region $REGION
+        --region ${REGION}
 fi
 
 # Do actions to the container instance.
 if [[ "$TERMINATE_STALE_EC2" = true ]]; then
     aws autoscaling terminate-instance-in-auto-scaling-group \
-        --instance-id $INSTANCE_ID \
+        --instance-id ${INSTANCE_ID} \
         --no-should-decrement-desired-capacity \
-        --region $REGION
+        --region ${REGION}
 else
     if [[ "$ENTER_STANDBY_STALE_EC2" = true ]]; then
         aws autoscaling enter-standby \
-            --instance-ids $INSTANCE_ID \
-            --auto-scaling-group-name $AUTOSCALING_GROUP_NAME \
+            --instance-ids ${INSTANCE_ID} \
+            --auto-scaling-group-name ${AUTOSCALING_GROUP_NAME} \
             --no-should-decrement-desired-capacity \
-            --region $REGION
+            --region ${REGION}
     fi
     if [[ "$DETACH_STALE_EC2" = true ]]; then
         aws autoscaling detach-instances \
-            --instance-ids $INSTANCE_ID \
-            --auto-scaling-group-name $AUTOSCALING_GROUP_NAME \
+            --instance-ids ${INSTANCE_ID} \
+            --auto-scaling-group-name ${AUTOSCALING_GROUP_NAME} \
             --no-should-decrement-desired-capacity \
-            --region $REGION
+            --region ${REGION}
     fi
 fi
 
